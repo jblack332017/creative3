15686d2d8cd7195fcfe8c4113bbed15d20da7060
Josh Black <joshblack@byu628796wks.app.byu.edu>
2017-03-02 13:55:57 -0700

    fixing changes

--- web.js
+++ web.js
@@ -22,23 +22,23 @@ angular.module('myApp', ['ngSanitize'])
             temp = temp.replace("#email",$scope.email);
             temp = temp.replace("#phoneNumber",$scope.phoneNumber);
 
-            temp = temp.replace("images/natureboy.jpg",$scope.pic1);
-            temp = temp.replace("images/girl_mountain.jpg",$scope.pic2);
-            temp = temp.replace("images/boy.jpg",$scope.pic3);
-            temp = temp.replace("images/man_bench.jpg",$scope.pic4);
-            temp = temp.replace("images/girl.jpg",$scope.pic5);
-            temp = temp.replace("images/girl_train.jpg",$scope.pic6);
+            // temp = temp.replace("images/natureboy.jpg",$scope.pic1);
+            // temp = temp.replace("images/girl_mountain.jpg",$scope.pic2);
+            // temp = temp.replace("images/boy.jpg",$scope.pic3);
+            // temp = temp.replace("images/man_bench.jpg",$scope.pic4);
+            // temp = temp.replace("images/girl.jpg",$scope.pic5);
+            // temp = temp.replace("images/girl_train.jpg",$scope.pic6);
 
-            temp = temp.replace("#info",$scope.info);
+            // temp = temp.replace("#info",$scope.info);
         
-            temp = temp.replace("#link1Title",$scope.link1Title);
-            temp = temp.replace("#link2Title",$scope.link2Title);
-            temp = temp.replace("#link1",$scope.link1);
-            temp = temp.replace("#link2",$scope.link2);
+            // temp = temp.replace("#link1Title",$scope.link1Title);
+            // temp = temp.replace("#link2Title",$scope.link2Title);
+            // temp = temp.replace("#link1",$scope.link1);
+            // temp = temp.replace("#link2",$scope.link2);
 
-            temp = temp.replace("#tag1",$scope.tag1);
-            temp = temp.replace("#tag2",$scope.tag2);
-            temp = temp.replace("#tag3",$scope.tag3);
+            // temp = temp.replace("#tag1",$scope.tag1);
+            // temp = temp.replace("#tag2",$scope.tag2);
+            // temp = temp.replace("#tag3",$scope.tag3);
 
             $scope.page = temp;
             $scope.html = heading+temp+ending;
